916c72a52970a37ff9b56fe08627a7c9b8f31153
Varnit-14 <64001643+Varnit-14@users.noreply.github.com>
2020-04-21 22:47:09 +0530

    Update assignment_1_(1).py

--- assignment_1_(1).py
+++ assignment_1_(1).py
@@ -52,6 +52,15 @@ print(l)
 * *  
 *
 """
+for i in range(1,6):
+    for j in range(i):
+        print("*",end="")
+    print()
+for k in range(4,0,-1):
+    for l in range(k):
+        print("*",end="")
+    print()
+    
 
 
 
